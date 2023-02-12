3569072d41550f4739b3c1df2f978796513dbe8d
Simon Rapilly <85184231+srapilly@users.noreply.github.com>
2023-02-12 16:05:41 +0100

    Use media query to know if it's already enabled or not (#2)
    * Use media query to know if it's already enabled or not
    
    * file host permission

--- background.js
+++ background.js
@@ -1,24 +1,30 @@
-const tabsEnabled = new Set();
 
 async function setForcedColors(debugee, value) {
-    await chrome.debugger.sendCommand(debugee, 'Emulation.setEmulatedMedia',
+    return await chrome.debugger.sendCommand(debugee, 'Emulation.setEmulatedMedia',
         {features: [{name: 'forced-colors', value: value}]})
+}
+
+async function forcedColorEnabled(tabId) {
+    const [{ result }] = await chrome.scripting
+        .executeScript({
+            target : {tabId : tabId},
+            func : () => window.matchMedia('(forced-colors: active)').matches,
+        })
+
+    return result
 }
 
 chrome.action.onClicked.addListener(async (tab) => {
     const tabId = tab.id
     const debugee = {tabId: tabId}
 
-    if (tabsEnabled.has(tabId)) {
-        await setForcedColors(debugee, 'none')
-        tabsEnabled.delete(tabId)
-    } else {
+    try {
         await chrome.debugger.attach(debugee, "1.3")
+    } catch { }
+
+    if (await forcedColorEnabled(tabId)) {
+        await setForcedColors(debugee, 'none')
+    } else {
         await setForcedColors(debugee, 'active')
-        tabsEnabled.add(tabId)
     }
 });
-
-chrome.debugger.onDetach.addListener(async (tab) => {
-    tabsEnabled.clear()
-})
