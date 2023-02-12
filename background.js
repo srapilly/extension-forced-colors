e17129ede8fdde71c43d0f2bf367e8f5936dd302
Simon Rapilly <simon.rapilly@doctolib.com>
2023-02-12 15:34:35 +0100

    Use media query to know if it's already enabled or not

--- background.js
+++ background.js
@@ -1,24 +1,35 @@
-const tabsEnabled = new Set();
 
 async function setForcedColors(debugee, value) {
-    await chrome.debugger.sendCommand(debugee, 'Emulation.setEmulatedMedia',
+    return await chrome.debugger.sendCommand(debugee, 'Emulation.setEmulatedMedia',
         {features: [{name: 'forced-colors', value: value}]})
+}
+
+async function forcedColorEnabled(tabId) {
+    const [{ result }] = await chrome.scripting
+        .executeScript({
+            target : {tabId : tabId},
+            func : () => window.matchMedia('(forced-colors: active)').matches,
+        })
+
+    return result
+}
+
+async function notAttached(tabId) {
+    return !(await chrome.debugger.getTargets())
+        .some(object => object.tabId === tabId && object.attached)
 }
 
 chrome.action.onClicked.addListener(async (tab) => {
     const tabId = tab.id
     const debugee = {tabId: tabId}
 
-    if (tabsEnabled.has(tabId)) {
-        await setForcedColors(debugee, 'none')
-        tabsEnabled.delete(tabId)
-    } else {
+    if (await notAttached(tabId)) {
         await chrome.debugger.attach(debugee, "1.3")
+    }
+
+    if (await forcedColorEnabled(tabId)) {
+        await setForcedColors(debugee, 'none')
+    } else {
         await setForcedColors(debugee, 'active')
-        tabsEnabled.add(tabId)
     }
 });
-
-chrome.debugger.onDetach.addListener(async (tab) => {
-    tabsEnabled.clear()
-})
